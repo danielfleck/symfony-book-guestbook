7e6a01e85fe24665e3f706b450ead49bc36fefab
Daniel Rodrigo Fleck <fleck@pm.pr.gov.br>
2021-06-07 16:55:27 -0300

    feat: acrescenta rota que recebe parametro

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,11 +9,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class ConferenceController extends AbstractController
 {
     #[Route('/conference', name: 'conference')]
-    #[Route('/', name: 'homepage')]
-    public function index(): Response
+    #[Route('/{name}', name: 'homepage')]
+    public function index(string $name = ""): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+        $greet = '';
+        if ($name) {
+          $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+        return new Response(<<<EOF
+          <html>
+            <body>
+              $greet <img src="images/under-construction.gif" />
+            </body>
+          </html>
+EOF
+        );
     }
 }
